cf4b04b350fcb031f4e6603c2a57db2062957ad6
Savchuk Sergiy <savchuksergiy94@gmail.com>
2021-03-17 11:33:41 +0200

    added code to the user model

--- EMS/DataModels/User.cs
+++ EMS/DataModels/User.cs
@@ -8,6 +8,9 @@ namespace DataModels
 {
     public class User
     {
-
+        public Guid Id { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public string Role { get; set; }
     }
 }
