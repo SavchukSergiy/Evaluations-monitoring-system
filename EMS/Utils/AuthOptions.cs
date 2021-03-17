f095e1ac7aaa643d774b420696fd5ae748b4a6f3
Savchuk Sergiy <savchuksergiy94@gmail.com>
2021-03-17 11:39:11 +0200

    added jwtbarier added auth option

---
+++ EMS/Utils/AuthOptions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Utils
+{
+    public class AuthOptions
+    {
+        public const string ISSUER = "MyAuthServer"; // token publisher
+        public const string AUDIENCE = "MyAuthClient"; // tocken client
+        const string KEY = "mysupersecret_secretkey!123";   // secure key
+        public const int LIFETIME = 1; // token lifetime - 1 minute
+        public static SymmetricSecurityKey GetSymmetricSecurityKey()
+        {
+            return new SymmetricSecurityKey(Encoding.ASCII.GetBytes(KEY));
+        }
+    }
+}
