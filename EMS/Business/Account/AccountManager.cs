ff8115de763d471a14e814df9cfecdacafe21445
Savchuk Sergiy <savchuksergiy94@gmail.com>
2021-03-22 09:50:04 +0200

    Added new manager for Account

---
+++ EMS/Business/Account/AccountManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Account
+{
+    public class AccountManager : IAccountManager
+    {
+        public void SignIn()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SignUp()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ EMS/Business/Account/IAccountManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Account
+{
+    /// <summary>
+    ///  IAccountManager
+    /// </summary>
+    public interface IAccountManager
+    {
+        /// <summary>
+        /// SignIn
+        /// </summary>
+        /// <returns></returns>
+        void SignIn();
+
+        /// <summary>
+        /// SignUp
+        /// <returns></returns>
+        void SignUp();
+    }
+}
