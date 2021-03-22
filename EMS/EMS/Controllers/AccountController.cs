cb0bedbc1df763a0c48a1f2775b2af716ec2e9ab
Savchuk Sergiy <savchuksergiy94@gmail.com>
2021-03-22 10:35:38 +0200

    Added ViewModels

--- EMS/EMS/Controllers/AccountController.cs
+++ EMS/EMS/Controllers/AccountController.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Utils;
 
 namespace EMS.Controllers
 {
@@ -11,5 +12,6 @@ namespace EMS.Controllers
     [ApiController]
     public class AccountController : ControllerBase
     {
+      
     }
 }
---
+++ EMS/ViewModels/RegisterModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace ViewModels
+{
+    public class RegisterModel
+    {
+        [Required(ErrorMessage = "Enter Email")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Enter Password")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Passwprd is wrong")]
+        public string ConfirmPassword { get; set; }
+    }
+}
